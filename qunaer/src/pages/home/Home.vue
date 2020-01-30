ea0bb04decb232bdf1b2ab305c557780a31851d4
nibaiqing <nbq4486@163.com>
2020-01-30 23:10:06 +0800

    project init

---
+++ qunaer/src/pages/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>你好</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Home',
+  data: function () {
+    return {
+
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
